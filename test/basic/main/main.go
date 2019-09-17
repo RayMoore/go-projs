eb232f40431429d3535706ce35bba87196f2f73f
molin <molinz@molins-MacBook-Pro.local>
2019-09-18 00:36:16 +0800

    global variables

--- test/basic/main/main.go
+++ test/basic/main/main.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
+// declare global variables
+var (
+	global_i1 = 10
+	global_s1 = "global string"
+	global_d1 = 10.11
+)
 // each go file needs a main func to exec
 // each folder is a package
 // each package should have only one main func
@@ -27,4 +33,7 @@ func main() {
 	fmt.Println("j =", j)
 	k := "this is a string" // omit "var" to declare a variable
 	fmt.Println("k =", k)
+	i1, s1, d1 := 10, "string", 10.11
+	fmt.Println("i1 =", i1, "s1 =", s1, "d1 =", d1)
+	fmt.Println("global variables =", global_i1, global_s1, global_d1)
 }
