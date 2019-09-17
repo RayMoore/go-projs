f8b2f48bbd0e1460c292c02c26e39942924e051f
molin <molinz@molins-MacBook-Pro.local>
2019-09-17 23:40:21 +0800

    first commit

---
+++ test/basic/main/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+// each go file needs a main func to exec
+// each folder is a package
+// each package should have only one main func
+func main() {
+
+	// this is single line comment
+	/*
+	 this is multi-line comments
+	 */
+
+	fmt.Println("hello go lang")
+}
